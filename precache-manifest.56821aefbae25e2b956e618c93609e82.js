afb84f4f6c8144ac3ae047bdc5fedc9322c7727a
mzlareb <mohammadlareb.zafar@nutanix.com>
2019-11-20 19:14:38 +0530

    Updates

---
+++ precache-manifest.56821aefbae25e2b956e618c93609e82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e5c347503d79add90d106517888da9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eb579aafc0f8d7229018",
+    "url": "/monsters-rolodex/static/css/main.13d49f53.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "eb579aafc0f8d7229018",
+    "url": "/monsters-rolodex/static/js/main.7eab5cb9.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
